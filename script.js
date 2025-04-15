97d04321f63a3a882c08ed0da9feceea9b63daea
DevIqra <eb20103029iqraawan@gmail.com>
2025-04-15 08:37:35 +0500

    done

--- script.js
+++ script.js
@@ -5,22 +5,35 @@
 // alert("IQRA1234");
 // alert("You're learning JavaScript!");
 // alert("Hello, i am displaying my alert message using JavaScript!");
-let myFavoriteColor = "blue";
-let luckyNumber; 
-luckyNumber = "Seven";
-let teamName = "Code Warriors";
-alert(teamName);
-var bestMan = "Charlie"; 
-bestMan = "Jordan";
-let caseQty;
-caseQty = 144;
-var num = 9;
-let total;
-total = 50 + 25;
-var merchTotal = 100;
-var shippingCharge = 10;
-var orderTotal = merchTotal + shippingCharge;
-console.log(orderTotal);
+// let myFavoriteColor = "blue";
+// let luckyNumber; 
+// luckyNumber = "Seven";
+// let teamName = "Code Warriors";
+// alert(teamName);
+// var bestMan = "Charlie"; 
+// bestMan = "Jordan";
+// let caseQty;
+// caseQty = 144;
+// var num = 9;
+// let total;
+// total = 50 + 25;
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
 
-let initialValue = 20; // Declared and assigned a number
-initialValue = initialValue + 15; 
+// let initialValue = 20; // Declared and assigned a number
+// initialValue = initialValue + 15; 
+var a = 10, b = 20, c = 30;
+// legal variable
+var userName;
+var _total;
+var $price;
+var first_name;
+var number123;
+// illegal 
+var 123number;    // Starts with a number
+var var;          // Reserved keyword
+var user-name;    // Hyphen not allowed
+var my variable;  // Space not allowed
+var !total; 
