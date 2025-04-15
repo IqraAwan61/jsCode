7e5d68433871e97228e41b2ca63b31eb12a72d96
DevIqra <eb20103029iqraawan@gmail.com>
2025-04-15 08:27:29 +0500

    js work uploaded

---
+++ script.js
@@ -0,0 +1,26 @@
+// alert("IQRA");
+// alert("AWAN");
+// alert("BIBIIQRA33@GMAIL.COM");
+// alert("8097346820736");
+// alert("IQRA1234");
+// alert("You're learning JavaScript!");
+// alert("Hello, i am displaying my alert message using JavaScript!");
+let myFavoriteColor = "blue";
+let luckyNumber; 
+luckyNumber = "Seven";
+let teamName = "Code Warriors";
+alert(teamName);
+var bestMan = "Charlie"; 
+bestMan = "Jordan";
+let caseQty;
+caseQty = 144;
+var num = 9;
+let total;
+total = 50 + 25;
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+let initialValue = 20; // Declared and assigned a number
+initialValue = initialValue + 15; 
